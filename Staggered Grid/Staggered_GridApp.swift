be0b0f4130cc1b966b2fe7e5f85a3517ce9d30bc
Tal Spektor <talspektor@gmail.com>
2022-04-18 21:38:26 +0300

    Initial Commit

---
+++ Staggered Grid/Staggered_GridApp.swift
@@ -0,0 +1,17 @@
+//
+//  Staggered_GridApp.swift
+//  Staggered Grid
+//
+//  Created by Tal talspektor on 18/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct Staggered_GridApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
